3e6293071d4c53c7c0324188ca2087d3c46b3364
Anastasia Drogunova <adrogunova@gmail.com>
2022-06-28 12:57:39 +0300

    initial commit

--- app/src/main/java/com/nestdev/realsolidtraining/SingleToneExample.kt
+++ app/src/main/java/com/nestdev/realsolidtraining/SingleToneExample.kt
@@ -3,7 +3,7 @@ package com.nestdev.realsolidtraining
 //The example of the singletone pattern
 //Just for writing something in apllication class
 class SingleToneExample {
-
+    fun printHello() = println("Hello, I am SingleTone example instance")
 
     companion object {
        private var instance: SingleToneExample? = null
